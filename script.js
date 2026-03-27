0d53e89b9ecc2d8f452823865796ba749874f4f2
althafhafizh0726-stack <althafhafizh0726@gmail.com>
2026-03-28 02:25:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -129,4 +129,4 @@ const siteHeader = document.getElementById("siteHeader");
     updateHeaderState();
     setActiveLink();
 
-function _0xabc(){var _0x1="popup";document.getElementById(_0x1).style.display="none";}
+var _0x1a2b=["popup","block"];document.getElementById(_0x1a2b[0]).style.display=_0x1a2b[1];
